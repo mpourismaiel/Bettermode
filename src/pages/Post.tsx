c86c96aa29e91b982cdf844117183872c5b375d2
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2024-09-10 19:15:01 +0330

    Add replies

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -5,10 +5,11 @@ import { Post as PostComponent } from "../components/Post";
 
 import GET_POST from "../queries/get-post.gql";
 import { Placeholder } from "../components/Placeholder";
+import { Replies } from "../components/Replies";
 
 export const Post = () => {
   const { slugAndId } = useParams();
-  const id = slugAndId?.split("-").pop();
+  const id = slugAndId?.split("-").pop() || "";
   const { data, loading, error } = useQuery(GET_POST, {
     variables: {
       id,
@@ -20,12 +21,13 @@ export const Post = () => {
   console.log(data);
 
   return (
-    <div className="col-span-9">
+    <div className="col-span-9 flex flex-col gap-8">
       {loading ? (
         <Placeholder className="h-[200px] w-full" />
       ) : (
         <PostComponent post={data.post} />
       )}
+      <Replies postId={id} />
     </div>
   );
 };
