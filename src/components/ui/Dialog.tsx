370cb6658d0e693e8be32571bb2f52f56b9b71ac
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2024-09-20 20:56:10 +0330

    Better dialog responsiveness

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -53,8 +53,8 @@ export const DialogContent = forwardRef<
         className={cn(
           "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] fixed left-[50%] top-[50%] z-50 flex translate-x-[-50%] translate-y-[-50%] flex-col gap-4 bg-surface-1 p-6 shadow-lg duration-200",
           {
-            "max-w-screen min-h-screen w-screen": isFullscreen,
-            "w-full max-w-lg border border-surface-3 sm:rounded-lg":
+            "max-w-screen min-h-screen w-screen border-0": isFullscreen,
+            "max-w-screen min-h-screen w-screen border-0 sm:w-full sm:max-w-lg sm:min-h-0 sm:border sm:border-surface-3 sm:rounded-lg":
               !isFullscreen,
           },
           className,
@@ -64,13 +64,13 @@ export const DialogContent = forwardRef<
         {children}
         <button
           onClick={toggleFullscreen}
-          className="absolute right-12 top-4 cursor-pointer rounded-sm text-foreground-2 opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none"
+          className="hidden sm:flex absolute right-12 top-4 cursor-pointer rounded-sm text-foreground-2 opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none"
         >
           <FullscreenIcon className="h-4 w-4" />
           <span className="sr-only">Full screen</span>
         </button>
         <DialogRadix.Close className="absolute right-4 top-4 rounded-sm text-foreground-2 opacity-70 transition-opacity hover:opacity-100 disabled:pointer-events-none">
-          <XIcon className="h-4 w-4" />
+          <XIcon className="h-8 w-8 sm:h-4 sm:w-4" />
           <span className="sr-only">Close</span>
         </DialogRadix.Close>
       </DialogRadix.Content>
